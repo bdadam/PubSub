2e552ab027cd4ed2cf42acbce87d982ff5baa488
bdadam <bdadam@gmail.com>
2014-01-05 09:43:55 +0100

    added "npm test" script + added repository to package.json

--- gruntfile.js
+++ gruntfile.js
@@ -58,6 +58,8 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', ['jshint', 'jasmine']);
 
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
